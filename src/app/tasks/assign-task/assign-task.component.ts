ab0108d0afb61564aa6e4d95471fe70296e17fde
vignesh vasanth <vigneshvasanth.c@trustrace.com>
2024-08-15 12:34:33 +0530

    transform template data with pipe is added on date pipe

--- src/app/tasks/assign-task/assign-task.component.ts
+++ src/app/tasks/assign-task/assign-task.component.ts
@@ -1,13 +1,14 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { ITasks } from './assign-task.model';
 import { CardComponent } from '../../shared/card/card.component';
+import { DatePipe } from '@angular/common';
 
 
 
 @Component({
   selector: 'app-assign-task',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, DatePipe],
   templateUrl: './assign-task.component.html',
   styleUrl: './assign-task.component.scss'
 })
